21ef8542e93e71092eae7c3e92ac235c65eb33b8
Robby Michael <Robby Michael>
2017-04-04 16:48:10 -0400

    initial commit

---
+++ ZombieSurvival/Player.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZombieSurvival
+{
+    class Player
+    {
+        public enum WeaponType
+        {
+            Rustyshovel = 0,
+            Revolver = 1,
+            Hunkofwood = 2,
+            Sturdybaseballbat = 3,
+            Handgrenade = 4,
+        }
+        public WeaponType Weapon { get; set; }
+        public int Health { get; set; }
+
+
+        Dictionary<string, int> Attacks = new Dictionary<string, int>
+            {
+                {"Punch", 5 },
+                {"Slash", 5 },
+                {"Chop", 35 },
+                {"Stab", 45 }
+            };
+
+        public Player(WeaponType weapon = WeaponType.Hunkofwood)
+        {
+            this.Weapon = weapon;
+        }
+        public WeaponType ChooseWeapon(int w)
+        {
+            switch (w)
+            {
+                case 0:
+                    Console.WriteLine("The rusty shovel looks like it could work for you.");
+                    return this.Weapon = WeaponType.Rustyshovel;
+                case 1:
+                    Console.WriteLine("You pick up the revolver, but it has no bullets....");
+                    return this.Weapon = WeaponType.Revolver;
+                case 2:
+                    Console.WriteLine("You pick up a hunk of wood.");
+                    return this.Weapon = WeaponType.Hunkofwood;
+                case 3:
+                    Console.WriteLine("The baseball bat seems like a good choice.");
+                    return this.Weapon = WeaponType.Sturdybaseballbat;
+                case 4:
+                    Console.WriteLine("The hand grenade seems safe...");
+                    return this.Weapon = WeaponType.Handgrenade;
+                default:
+                    Console.WriteLine("You could not decide what you wanted so you grab the Hunk of Wood! Good Luck!");
+                    return this.Weapon = WeaponType.Hunkofwood;
+
+            }
+        }
+        public void HelthDisplay()
+        {
+            Console.WriteLine("Your health is at {0}.", this.Health);
+        }
+        public void death()
+        {
+            Console.WriteLine("You are Dead.");
+        }
+    }
+}
---
+++ ZombieSurvival/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZombieSurvival
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Zombie Survival";
+            MainAppTitle();
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.WriteLine("You wake up in a dark basement and are covered in blood.");
+            Console.WriteLine("In the basement you find a flashlight, a healthy meal, and some weapons");
+            Console.WriteLine("You obviously need the flashlight because it is dark, and the meal because you are starving.");
+            Console.WriteLine("You can only cary one weapon. What do you Choose?\n" +
+                               "\n" + 
+                              "==================================================\n" +
+                              "0: The Rusty Shovel\n" +
+                              "1: A Revolver\n" +
+                              "2: A hunk of Wood\n" +
+                              "3: A Sturdy Baseball Bat\n" +
+                              "4: A Handgrenade\n");
+            int weaponType = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Player player = new Player();
+            player.ChooseWeapon(weaponType);
+
+            Console.WriteLine("You are now prepared to leave. There is a door with some light shining from under it.\n" +
+                "Would you like to go through the door? y/n");
+            string userAnswer = Console.ReadLine();
+            Console.WriteLine("====================================================");
+
+            if(userAnswer == "n")
+            {
+                Console.WriteLine("test");
+            }
+            Console.ReadLine();
+
+            
+        }
+        private static void MainAppTitle()
+        {
+            Console.Clear();
+            string appTitle = @"
+                                                                     
+           @@@@@@@@   @@@@@@   @@@@@@@@@@   @@@@@@@   @@@  @@@@@@@@  
+           @@@@@@@@  @@@@@@@@  @@@@@@@@@@@  @@@@@@@@  @@@  @@@@@@@@  
+                @@!  @@!  @@@  @@! @@! @@!  @@!  @@@  @@!  @@!       
+               !@!   !@!  @!@  !@! !@! !@!  !@   @!@  !@!  !@!       
+              @!!    @!@  !@!  @!! !!@ @!@  @!@!@!@   !!@  @!!!:!    
+             !!!     !@!  !!!  !@!   ! !@!  !!!@!!!!  !!!  !!!!!:    
+            !!:      !!:  !!!  !!:     !!:  !!:  !!!  !!:  !!:       
+           :!:       :!:  !:!  :!:     :!:  :!:  !:!  :!:  :!:       
+            :: ::::  ::::: ::  :::     ::    :: ::::   ::   :: ::::  
+           : :: : :   : :  :    :      :    :: : ::   :    : :: ::   
+                                                                     
+                                                                             
+   @@@@@@   @@@  @@@  @@@@@@@   @@@  @@@  @@@  @@@  @@@   @@@@@@   @@@       
+  @@@@@@@   @@@  @@@  @@@@@@@@  @@@  @@@  @@@  @@@  @@@  @@@@@@@@  @@@       
+  !@@       @@!  @@@  @@!  @@@  @@!  @@@  @@!  @@!  @@@  @@!  @@@  @@!       
+  !@!       !@!  @!@  !@!  @!@  !@!  @!@  !@!  !@!  @!@  !@!  @!@  !@!       
+  !!@@!!    @!@  !@!  @!@!!@!   @!@  !@!  !!@  @!@  !@!  @!@!@!@!  @!!       
+   !!@!!!   !@!  !!!  !!@!@!    !@!  !!!  !!!  !@!  !!!  !!!@!!!!  !!!       
+       !:!  !!:  !!!  !!: :!!   :!:  !!:  !!:  :!:  !!:  !!:  !!!  !!:       
+      !:!   :!:  !:!  :!:  !:!   ::!!:!   :!:   ::!!:!   :!:  !:!   :!:      
+  :::: ::   ::::: ::  ::   :::    ::::     ::    ::::    ::   :::   :: ::::  
+  :: : :     : :  :    :   : :     :      :       :       :   : :  : :: : :  
+
+              +-+-+-+-+-+ +-+-+-+ +-+-+-+ +-+-+ +-+-+-+-+ +-+-+-+
+              |P|r|e|s|s| |A|n|y| |K|e|y| |T|o| |W|a|k|e| |U|p|!|
+              +-+-+-+-+-+ +-+-+-+ +-+-+-+ +-+-+ +-+-+-+-+ +-+-+-+";
+ 
+
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(appTitle);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
